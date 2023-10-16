35771f268e9f6f5dc106abb0c513b3d763919276
Sergey <klapischeff@yandex.ru>
2023-10-16 19:25:02 +0300

    Добавьте файлы проекта.

---
+++ MatrixSort/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Дан двумерный массив.
+732
+496
+185
+
+Отсортировать данные в нем по возрастанию.
+123
+456
+789*/
+
+using System.Collections;
+
+namespace MatrixSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //дана матрица
+            int[,] matrix = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+
+            // создаём одномерный массив
+            int[] array = new int[matrix.GetLength(0) * matrix.GetLength(1)]; 
+            int indexArr = 0;
+
+            //записываем значения матрици в одномерный массив
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    array[indexArr] = matrix[i, j];
+                    indexArr++;
+                }
+            }
+
+            //сортируем одномерный массив
+            Array.Sort(array);
+            indexArr = 0;
+
+            //Перезаписываем значения матрицы, значениями из отсортированного одномерного массива
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    matrix[i, j] = array[indexArr];
+                    Console.Write(matrix[i, j]);
+                    indexArr++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
